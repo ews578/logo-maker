b24d09e15b6061515d540db09b4420d4b98d506b
ews578 <ewsnider28@yahoo.com>
2024-02-26 11:09:27 -0600

    finished shapes test js and test logo maker

--- shapes.test.js
+++ shapes.test.js
@@ -1,9 +1,29 @@
 const {Triangle} = require("./shapes");
 
 describe("Triangle", () => {
-test("renders a triangle with the correct information", () => {
-    const testSvg = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><polygon points="150, 18 244, 182 56, 182" fill="Blue" /><text x="150" y="150" font-size="60" text-anchor="middle" fill="Green">EWS</text> </svg>'
-    const svg = new Triangle ("Blue","EWS","Green")
-    expect(testSvg).toEqual(svg.render())
-})
-})
+    test("renders a triangle with the correct information", () => {
+        const testSvg = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><polygon points="150, 18 244, 182 56, 182" fill="Blue" /><text x="150" y="150" font-size="60" text-anchor="middle" fill="Green">EWS</text> </svg>'
+        const svg = new Triangle ("Blue","EWS","Green")
+         expect(testSvg).toEqual(svg.render())
+    })
+    })
+
+const {Circle} = require("./shapes")
+
+describe("Circle", () => {
+    test("renders a circle with the correct information", () => {
+        const testSvg = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><polygon points="150, 18 244, 182 56, 182" fill="Blue" /><text x="150" y="150" font-size="60" text-anchor="middle" fill="Green">EWS</text> </svg>'
+        const svg = new Circle ("Blue","EWS","Green")
+        expect(testSvg).toEqual(svg.render())
+    })
+    })
+
+const {Square} = require("./shapes")
+
+describe("Square", () => {
+    test("renders a square with the correct information", () => {
+        const testSvg = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><polygon points="150, 18 244, 182 56, 182" fill="Blue" /><text x="150" y="150" font-size="60" text-anchor="middle" fill="Green">EWS</text> </svg>'
+        const svg = new Square ("Blue","EWS","Green")
+        expect(testSvg).toEqual(svg.render())
+    })
+    })
